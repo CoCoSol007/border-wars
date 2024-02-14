6abd0f0c87e34f00064a7cb2f96df4c7ba252ca9
Raphaël <r.lauray@outlook.fr>
2024-02-14 01:49:11 +0100

    WIP - Beggining of the menu ui, setup nodes

--- crates/border-wars/src/scenes/menu.rs
+++ crates/border-wars/src/scenes/menu.rs
@@ -1,7 +1,6 @@
 //! The main menu of the game.
 
 use bevy::prelude::*;
-use bevy_egui::{egui, EguiContexts};
 
 use crate::CurrentScene;
 
@@ -10,36 +9,103 @@ pub struct MenuPlugin;
 
 impl Plugin for MenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, menu_ui.run_if(in_state(CurrentScene::Menu)));
+        app.add_systems(Startup, menu_ui.run_if(in_state(CurrentScene::Menu)));
     }
 }
 /// Display the UI of the menu to host a game or join one.
-fn menu_ui(
-    mut ctx: EguiContexts,
-    mut connection_string: Local<String>,
-    mut next_scene: ResMut<NextState<CurrentScene>>,
-) {
-    egui::CentralPanel::default().show(ctx.ctx_mut(), |ui| {
-        ui.heading("Border Wars");
-
-        ui.separator();
-
-        ui.label("Connect to an existing game:");
-        ui.horizontal(|ui| {
-            ui.label("Game ID: ");
-            ui.text_edit_singleline(&mut *connection_string);
-
-            if ui.button("Join").clicked() {
-                next_scene.set(CurrentScene::Game);
-                // TODO: connect to the game
-            }
+fn menu_ui(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+    // Green
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                margin: UiRect {
+                    left: (Val::Auto),
+                    right: (Val::Auto),
+                    top: (Val::Px(0.)),
+                    bottom: (Val::Px(0.)),
+                },
+                width: Val::Percent(55.),
+                height: Val::Percent(100.),
+                flex_direction: FlexDirection::Column,
+                ..default()
+            },
+            ..default()
+        })
+        // Text
+        .with_children(|parent| {
+            parent.spawn(
+                NodeBundle{
+                    style: Style {
+                        margin: UiRect {
+                            left: (Val::Auto),
+                            right: (Val::Auto),
+                            top: (Val::Percent(5.)),
+                            bottom: (Val::Percent(15.)),
+                        },
+                        width: Val::Percent(90.),
+                        height: Val::Percent(25.),
+                        ..default()
+                    },
+                    background_color: BackgroundColor(Color::RED),
+                    ..default()
+                }
+                
+            );
+        })
+        // BLUE
+        .with_children(|parent| {
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        flex_direction: FlexDirection::Column,
+                        margin: UiRect {
+                            left: (Val::Auto),
+                            right: (Val::Auto),
+                            top: (Val::Auto),
+                            bottom: (Val::Percent(5.)),
+                        },
+                        width: Val::Percent(85.),
+                        height: Val::Percent(70.),
+                        ..default()
+                    },
+                    ..default()
+                })
+                // YELLOW_GREEN 1
+                .with_children(|parent| {
+                    parent.spawn(NodeBundle {
+                        style: Style {
+                            width: Val::Percent(100.),
+                            height: Val::Percent(45.),
+                            margin: UiRect {
+                                left: (Val::Auto),
+                                right: (Val::Auto),
+                                top: (Val::Auto),
+                                bottom: (Val::Auto),
+                            },
+                            ..default()
+                        },
+                        background_color: BackgroundColor(Color::YELLOW_GREEN),
+                        ..default()
+                    });
+                })
+                // YELLOW_GREEN 2
+                .with_children(|parent| {
+                    parent.spawn(NodeBundle {
+                        style: Style {
+                            width: Val::Percent(100.),
+                            height: Val::Percent(45.),
+                            margin: UiRect {
+                                left: (Val::Auto),
+                                right: (Val::Auto),
+                                top: (Val::Auto),
+                                bottom: (Val::Auto),
+                            },
+                            ..default()
+                        },
+                        background_color: BackgroundColor(Color::YELLOW_GREEN),
+                        ..default()
+                    });
+                });
         });
-
-        ui.separator();
-
-        if ui.button("Create new game").clicked() {
-            next_scene.set(CurrentScene::Lobby);
-            // TODO: create a new game
-        }
-    });
 }
