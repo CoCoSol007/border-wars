5d520c917f711b9476977ef459eb6892bafee53d
CoCo_Sol <solois.corentin@gmail.com>
2024-04-03 15:44:17 +0000

    Add the ownership system (#96)
    closes: #67
    Reviewed-on: https://git.tipragot.fr/fish-cannard/border-wars/pulls/96
    Reviewed-by: Raphaël <r.lauray@outlook.fr>
    Co-authored-by: CoCo_Sol <solois.corentin@gmail.com>
    Co-committed-by: CoCo_Sol <solois.corentin@gmail.com>

--- crates/border-wars/src/main.rs
+++ crates/border-wars/src/main.rs
@@ -3,6 +3,7 @@
 use bevy::prelude::*;
 use border_wars::camera::CameraPlugin;
 use border_wars::map::generation::MapGenerationPlugin;
+use border_wars::map::ownership::OwnershipPlugin;
 use border_wars::map::renderer::RendererPlugin;
 use border_wars::map::selected_tile::SelectTilePlugin;
 use border_wars::networking::NetworkingPlugin;
@@ -19,5 +20,6 @@ fn main() {
         .add_plugins(NetworkingPlugin)
         .add_plugins(MapGenerationPlugin)
         .add_plugins(UiPlugin)
+        .add_plugins(OwnershipPlugin)
         .run();
 }
